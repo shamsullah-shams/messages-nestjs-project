0bf0683ade4e2ecf70e9ee8b1d9f68152c1ca297
shamsullah-shams <shamsullah07073@gmail.com>
2024-08-11 17:28:50 +0430

    dependancy injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -4,11 +4,7 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messageService: MessagesService;
-
-  constructor() {
-    this.messageService = new MessagesService();
-  }
+  constructor(public messageService: MessagesService) {}
 
   @Get()
   listMessages() {
